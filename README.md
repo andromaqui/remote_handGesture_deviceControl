f2e69d8fcbf4ae925d799b70b09618c4436ddaf3
Andromachi Rozaki <andromachirozakii@gmail.com>
2021-02-02 20:25:48 +0100

    upadated GUI and README.md

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 
 This is a project being developed as part of my Bachelors thesis aiming to combine thea areas of Machine Learning, Computer Vision and Web Development. The app detects hand gestures from the user and sends the respective HTTP request to LIFX api.
 
+![Alt Text](demo.gif)
+
 
 
 ## Modules
-
+- gui : the graphical user interface of the app 
 - backend : responsible for sending requests to LIFX api in order to control the Smart Bulb
+- training : includes the dataset used as well as the several training scripts and the model architectures, which were used for this project
+
+## TODO
+- document data collection and training process
+- update evaluation.ipynb
--- gui/app.py
+++ gui/app.py
@@ -8,10 +8,10 @@ from api_requests import turn_off, turn_on
 
 ## if probability less than theshold
 ## count as no class
-PROBABILITY_THRESHOLD = 0.5
+PROBABILITY_THRESHOLD = 0.6
 
 ## frames per second
-TOTALFRAMES_PERSECOND = 20
+TOTALFRAMES_PERSECOND = 10
 
 
 blocking = True
@@ -39,30 +39,43 @@ counter = 0
 ## TODO
 ##calculate FPS
 ##
+isOff = 0
+isOn = 0
+
 
 ### take the last 20 frames -> for each frame map a class -> take most common value
 while(1):
     turnOn_thread = threading.Thread(target=turn_on, name="turnBulbOn", args=[])
     turnOff_thread = threading.Thread(target=turn_off, name="turnBulbOff", args=[])
+    
 
     ## RESET FRAME COUNT
     if(currentFrame == TOTALFRAMES_PERSECOND):
         currentFrame = 0
         #print(classPerFrame)
         averageFrameClass = max(set(classPerFrame), key=classPerFrame.count)
-
-        if(averageFrameClass == 0):
-            logging.info("Thread to TURN ON bulb started")
-            turnOn_thread.start()
-        elif(averageFrameClass == 3):
-            logging.info("Thread to TURN OFF bulb started")
-            turnOff_thread.start()
-
+        print(averageFrameClass)
+        try:
+            if(averageFrameClass.item()==1 and isOn==0):
+                print("turn on")
+                is0n = 1
+                isOff = 0
+                turnOn_thread.start() 
+                #turn_on()
+                #logging.info("Thread to TURN ON bulb started")
+            elif(averageFrameClass.item()==3 and isOff==0):
+                print("turn off")
+                is0ff = 1
+                is0n = 0
+                #logging.info("Thread to TURN OFF bulb started")
+                turnOff_thread.start()
+        except Exception as e:    
+            print(e)   
         #print(averageFrameClass)
-
-    ret, frame = cam.read()
-    currentFrame+=1
+    
+    ret, frame = cam.read()    
     if ret:
+        currentFrame += 1
         frame = cv2.flip(frame,1)
         display = cv2.rectangle(frame.copy(),(startX,startY),(finishX,finishY),(0,0,255),2) 
         cv2.imshow('Total Input',display)
@@ -71,13 +84,14 @@ while(1):
 
         #cv2.imwrite("A" + str(counter)+".jpg", ROI) 
         #counter+=1
-        """    
+          
+        
          ## add autocontrast
-        img = Image.fromarray(display)
+        img = Image.fromarray(ROI)
         # applying autocontrast method  
         img = ImageOps.autocontrast(img, cutoff = 1, ignore = 1) 
         numpyimg = np.array(img)
-        """
+        
 
         img = cv2.resize(ROI, (128, 128)) #R
         img = img.reshape(1, 128, 128, 3)
@@ -87,10 +101,38 @@ while(1):
 
         predicted_label = np.argwhere(predictions > PROBABILITY_THRESHOLD) # Get index of the predicted label from prediction
         try:
-            print(predicted_label[0][0])
+            #print(predicted_label[0][0])
             classPerFrame.append(predicted_label[0][0])
+            """
+            classItem = predicted_label[0][0]
+            
+            print(classItem)
+            if((classItem.item())==0 and isOn==0):
+                print("turn on")
+                is0n = 1
+                isOff = 0
+                turnOn_thread.start() 
+                #turn_on()
+                #logging.info("Thread to TURN ON bulb started")
+            elif(classItem.item() == 3 and isOn==0):
+                print("turn off")
+                is0ff = 1
+                is0n = 0
+                #turn_off()
+                #logging.info("Thread to TURN OFF bulb started")
+                turnOff_thread.start()
+            else:
+                print(classItem)
+            """
         except Exception as e:
-            print(str(counter)+predicted_label)
+                continue
+                """
+                print("Exception")
+                print(counter)
+                print(predicted_label)
+                classPerFrame.append(None)
+                """
+
 
         if cv2.waitKey(10) & 0xFF == ord('q'):
           break
